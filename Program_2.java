0d67eddea410939e5442c0aa3e9c9fabb74ec468
suniljakkula <sunij8911@gmail.com>
2023-02-26 12:13:03 +0530

    HashMap key,values forEach

--- Program_2.java
+++ Program_2.java
@@ -7,9 +7,13 @@ public class Program_2 {
         Map<String,String> map=new HashMap<>();
         map.put("PM","Prime Minister");
         map.put("CM","Chief Minister");
+        //Before Java we used to iterate the key value pairs from Hash Map
         for(Map.Entry<String,String> entry:map.entrySet()){
             System.out.println(entry.getKey()+" - "+entry.getValue());
         }
+        //After Java8 it is easier to iterate the values.
+        System.out.println("We are iterating the HashMap values using java Java8");
+        map.forEach((key,value)->System.out.println(key+" - "+value));
 
     }
 }
