4ec16afd952d92d5069325f8fa20aa9ae252dc5a
suniljakkula <sunij8911@gmail.com>
2023-03-03 07:29:29 +0530

    streams api code committed

--- Program_3.java
+++ Program_3.java
@@ -1,25 +1,32 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program_3 {
-    private static List<String> listOfFruits=Arrays.asList("Banana","Apple","Kiwi","Grapes");
+    private static final List<String> listOfFruits=Arrays.asList("Banana","Apple","Kiwi","Grapes");
 
     public static void main(String[] args) {
         List<String> result=getFilterFruitsDetails(listOfFruits,"Banana");
         for(String fruit:result){
             System.out.println(fruit);
         }
+        System.out.println("We are used streams and reduced the no.of lines of code");
+        List<String> fruitsFilter = listOfFruits.stream()
+                .filter(fruit -> !"Kiwi".equals(fruit))
+                .collect(Collectors.toList());
+        System.out.println(fruitsFilter);
     }
-
+    //we will reduce the no.of lines of code using the java8
     public static List<String> getFilterFruitsDetails(List<String> fruits,String filter){
         List<String> listOfFruits=new ArrayList<>();
         for(String fruit:fruits){
-            if(!"Banana".equals(fruit)){
+            if(!filter.equals(fruit)){
                 listOfFruits.add(fruit);
             }
         }
         return listOfFruits;
     }
-
 }
