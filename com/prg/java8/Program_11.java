4a57bb0c3068b432a1ff95b592cfcbfa63586e97
suniljakkula <sunij8911@gmail.com>
2023-03-05 22:08:14 +0530

    String joiner class

--- com/prg/java8/Program_11.java
+++ com/prg/java8/Program_11.java
@@ -1,8 +1,16 @@
 package com.prg.java8;
 
+import java.util.StringJoiner;
+
 public class Program_11 {
 
     public static void main(String[] args) {
+        StringJoiner joiner=new StringJoiner(",");
+        joiner.add("Hello");
+        joiner.add("Welcome");
+        joiner.add("To");
+        joiner.add("StringJoiner");
+        System.out.println(joiner);
 
 
     }
