0715937c98f9a328ce9df42746b7857a9d439544
suniljakkula <sunij8911@gmail.com>
2023-04-21 21:11:07 +0530

    sort the hashmap based on values

--- SortTheHashMapValues.java
+++ SortTheHashMapValues.java
@@ -2,19 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static java.util.Map.*;
+import static java.util.Map.Entry.comparingByValue;
 
 public class SortTheHashMapValues {
 
     public static void main(String[] args) {
         HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("Java", 5);
+        hashMap.put("Java", 2);
         hashMap.put("Hibernate", 1);
         hashMap.put("C", 4);
-        hashMap.put("Spring", 2);
+        hashMap.put("Spring", 5);
         hashMap.put("Microservices", 3);
 
-        Stream<Map.Entry<String, Integer>> set = hashMap.entrySet().stream().sorted(Entry.comparingByValue());
+        Stream<Map.Entry<String, Integer>> set = hashMap.entrySet().stream().sorted(comparingByValue());
         set.forEach(System.out::println);
     }
 }
