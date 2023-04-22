cb1ae02117f708e8f1ed1e72081038e2d7581af8
suniljakkula <sunij8911@gmail.com>
2023-04-22 07:00:01 +0530

    sort the hashmap based values

--- SortTheHashMapValues.java
+++ SortTheHashMapValues.java
@@ -1,6 +1,4 @@
 import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
 
 import static java.util.Map.Entry.comparingByValue;
 
@@ -11,10 +9,12 @@ public class SortTheHashMapValues {
         hashMap.put("Java", 2);
         hashMap.put("Hibernate", 1);
         hashMap.put("C", 4);
+        hashMap.put("SpringBoot",6);
         hashMap.put("Spring", 5);
         hashMap.put("Microservices", 3);
 
-        Stream<Map.Entry<String, Integer>> set = hashMap.entrySet().stream().sorted(comparingByValue());
-        set.forEach(System.out::println);
+
+        hashMap.entrySet().stream().sorted(comparingByValue()).forEach(System.out::println);
+
     }
 }
